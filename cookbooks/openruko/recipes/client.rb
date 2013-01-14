7160024e8f60868bc970a6a430f9bc5e9cad026e
Thomas Buckley-Houston <tom@tombh.co.uk>
2013-01-14 11:53:34 +0000

    Stop Postgres before removing. Full revision hashes for dependencies

--- cookbooks/openruko/recipes/client.rb
+++ cookbooks/openruko/recipes/client.rb
@@ -2,7 +2,7 @@ script "install-slotbox-cli" do
   interpreter "bash"
 
   code <<-EOF
-    gem install slotbox --no-rdoc --no-ri --version #{node["versions"]["slotbox-cli"]}
+    GEM_HOME=/home/rukosan/.gem/ruby/1.9.1 gem install slotbox --no-rdoc --no-ri --version #{node["versions"]["slotbox-cli"]}
   EOF
 
   not_if "gem which slotbox"
