4b9b3cea770f63ef58dc2179bf60433b70620026
Thomas Buckley-Houston <tom@tombh.co.uk>
2013-01-15 23:11:28 +0000

    Hadn't set recipe to use slotbox version of dynohost

--- cookbooks/openruko/recipes/dynohost.rb
+++ cookbooks/openruko/recipes/dynohost.rb
@@ -1,7 +1,7 @@
 git "/home/rukosan/openruko/dynohost" do
   user "rukosan"
   group "rukosan"
-  repository "https://github.com/openruko/dynohost.git"
+  repository "https://github.com/slotbox/dynohost.git"
   action :checkout
   revision node["versions"]["dynohost"]
 end
